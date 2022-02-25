0a963be0c1acaf24d03aa4747eef9adf612b2999
jiwonjung <4dakto@gmail.com>
2022-02-26 00:18:28 +0900

    add get all todos

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private todoService: TodosService) {}
+
+  @Get()
+  getAllBoards() {
+    return this.todoService.getAllBoards();
+  }
+}
