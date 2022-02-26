d8fb0e3f52adb3164f76ca55623454f841eee1eb
jiwonjung <4dakto@gmail.com>
2022-02-26 21:56:51 +0900

    code tweak

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -21,43 +21,8 @@ import { Todo } from './todo.entity';
 export class TodosController {
   constructor(private todoService: TodosService) {}
 
-  // @Get('/')
-  // getAllBoards(): Todo[] {
-  //   return this.todoService.getAllBoards();
-  // }
-
   @Get('/:id')
   getTodoById(@Param('id') id: number): Promise<Todo> {
     return this.todoService.getTodoById(id);
   }
-
-  // @Get('/:id')
-  // getTodoById(@Param('id') id: string): Todo {
-  //   const found = this.todoService.getTodoById(id);
-  //   if (isUndefined(found)) {
-  //     throw new NotFoundException(`Cant't find Todo with id - ${id}`);
-  //   }
-  //
-  //   return found;
-  // }
-  //
-  // @Post()
-  // @UsePipes(ValidationPipe)
-  // createTodo(@Body() createTodoDto: CreateTodoDto): Todo {
-  //   return this.todoService.createTodo(createTodoDto);
-  // }
-  //
-  // @Delete('/:id')
-  // deleteTodoById(@Param('id') id: string): void {
-  //   const found = this.getTodoById(id);
-  //   return this.todoService.deleteTodoById(found.id);
-  // }
-  //
-  // @Patch('/:id/status')
-  // updateTodoStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', TodoStatusValidationPipe) status: TodoStatus,
-  // ): Todo {
-  //   return this.todoService.updateTodoStatus(id, status);
-  // }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -13,23 +13,6 @@ export class TodosService {
     @InjectRepository(TodoRepository)
     private todoRepository: TodoRepository,
   ) {}
-  // getAllBoards(): Todo[] {
-  //   return this.todos;
-  // }
-  //
-  // createTodo(createTodoDto: CreateTodoDto): Todo {
-  //   const { title, description } = createTodoDto;
-  //   const todo: Todo = {
-  //     id: uuid(),
-  //     title,
-  //     description,
-  //     status: TodoStatus.OPEN,
-  //   };
-  //
-  //   this.todos.push(todo);
-  //
-  //   return todo;
-  // }
 
   async getTodoById(id: number): Promise<Todo> {
     const found = await this.todoRepository.findOne(id);
@@ -39,19 +22,4 @@ export class TodosService {
 
     return found;
   }
-
-  // getTodoById(id: string): Todo {
-  //   return this.todos.find((todo) => todo.id === id);
-  // }
-  //
-  // deleteTodoById(id: string): void {
-  //   this.todos = this.todos.filter((todo) => todo.id !== id);
-  // }
-  //
-  // updateTodoStatus(id: string, status: TodoStatus): Todo {
-  //   const todo = this.getTodoById(id);
-  //   todo.status = status;
-  //
-  //   return todo;
-  // }
 }
