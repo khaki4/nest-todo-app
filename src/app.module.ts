16efe4a42c3531c98c29d18b51c6a3e8f775486d
jiwonjung <4dakto@gmail.com>
2022-02-26 23:06:36 +0900

    ADD Auth module controller service repository entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), TodosModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), TodosModule, AuthModule],
 })
 export class AppModule {}
